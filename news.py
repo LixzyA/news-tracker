9453c1ad4ea5f6f8b59c308325aa19e401b2e1ff
Felix <felix.antony168@gmail.com>
2026-05-30 15:46:30 +0700

    implemented logging and send email

--- news.py
+++ news.py
@@ -3,12 +3,17 @@ import requests
 from model import ClassifiedNews, NewsSource
 from database import query_news, insert_news
 from huggingface_hub import InferenceClient
+from logger import setup_logger
+import resend
+import time
+
 import os
 from dotenv import load_dotenv
 load_dotenv()
 client = InferenceClient(api_key=os.getenv("HF_TOKEN"))
 model = os.getenv("LLM_MODEL", "google/gemma-4-26B-A4B-it")
 prompt = open("prompt.txt", "r").read()
+logger = setup_logger(__name__)
 
 def get_news(source: NewsSource, type: str | None = None):
     base_url = f"https://berita-indo-api-next.vercel.app/api/{source.value}"
@@ -28,6 +33,9 @@ def classify_news(news: dict) -> dict:
     """
     Classify whether news will highly impact the stock market.
     """
+    if not news.get("contentSnippet"):
+        logger.exception("content snippet / description is missing for news item with link: %s", news.get("link"))
+        raise ValueError("content snippet / description is required for classification")
     content_snippet = news["contentSnippet"].lower()
     global model
     response_format = {
@@ -59,32 +67,65 @@ def classify_news(news: dict) -> dict:
             structured_data = completion.choices[0].message.content
             return json.loads(structured_data)
         except Exception as e:
-            print(f"Attempt {attempt + 1} failed: {e}")
+            print(f"Classification attempt {attempt + 1} failed: {e}")
             if attempt == max_retries - 1:
-                print("All attempts failed.")
+                logger.error("All classifications attempts failed.")
     return {
         "is_highly_impactful": False,
         "reasoning": "Failed to classify news after multiple attempts.",
         "confidence": 0.0
     }
 
-if __name__ == "__main__":
-    raw_data = get_news(NewsSource.CNBC)
-    if not raw_data:
-        print("No data found")
-        exit()
+# TODO: needs a dns verification to send email from custom domain
+def send_email(subject: str, body: str, to: str | list[str]):
+    resend_api_key = os.getenv("RESEND_API_KEY")
+    try:
+        result = resend.Emails.send({
+            "from": "Felix <onboarding@mail.felix-antony.com>",
+            "to": to,
+            "subject": subject,
+            "html": body
+        })
+        return result
+    except Exception as e:
+        logger.error(f"Failed to send email: {e}")
 
-    news = raw_data["data"]
-    exists = 0
-    classified = 0
-    for item in news[:1]: 
-        news_link = item["link"]
-        if query_news(news_link):
-            print(f"News with link '{news_link}' already exists in the database.")
-            exists += 1
+def main():
+    important_news = []
+    for source in NewsSource:
+        logger.info(f"Fetching news from {source.value}...")
+        raw_data = get_news(source)
+        if not raw_data:
+            logger.warning(f"No data found for {source.value}")
             continue
-        classified_result = classify_news(item)
-        classified += 1
-        result = insert_news(item, source=NewsSource.CNBC.value, classified_data=classified_result)
-    print(f"Classified : {classified} news, {exists} news already exists in the database.")
-            
+
+        news = raw_data["data"]
+        if source == NewsSource.REPUBLIKA:
+            for item in news:
+                item['contentSnippet'] = item.pop('description')
+
+        for item in news[:10]:  
+            news_link = item["link"]
+            if query_news(news_link):
+                logger.info(f"News with link '{news_link}' already exists in the database.")
+                continue
+            classified_result = classify_news(item)
+            if classified_result.get("is_highly_impactful"):
+                important_news.append(item)
+            insert_news(item, source=source.value, classified_data=classified_result)
+    targets = ['felix.antony168@gmail.com']
+    if important_news:
+        send_email(
+            subject="Daily Stock Market Impactful News",
+            body = "<h1>Here are the news that are classified as highly impactful to the stock market:</h1>" + 
+                "".join([f"<p><a href='{news['link']}'>{news['title']}</a></p>" for news in important_news]),
+            to = targets
+        )
+    
+
+if __name__ == "__main__":
+    logger.info("Starting news tracker process...")
+    while True:
+        main()
+        logger.info("News tracker process completed. Sleeping for 5 minutes...")
+        time.sleep(300)
